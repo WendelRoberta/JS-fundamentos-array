cd0b243c0529156defd8fa8826fac2ad69491832
wendelroberta <wendel.roberta1995@gmail.com>
2022-03-09 22:26:31 -0300

    Concluindo curso fundamentos js: arrays

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const arrayNums = [1, 2, 3, 4]
+// const arrayNums = [1, 2, 3, 4]
 
-const resultados = arrayNums.map(Num => Num*10);
-console.log(resultados);
+// const resultados = arrayNums.map(Num => Num*10);
+// console.log(resultados);
 
 // function multiplicaPorDez(num) {
 //     return num * 10
@@ -9,3 +9,21 @@ console.log(resultados);
 // const arraySomada = arrayNums.map(multiplicaPorDez)
 
 // console.log(arraySomada)
+
+ const numeros = [43, 50, 65, 12]
+
+/* const soma = numeros.reduce((acum, atual) => atual + acum, 0)
+
+console.log(soma) //170  */
+
+/* const soma = numeros.reduce(function (acum, atual) {
+    return atual + acum
+   }, 0) */
+
+function operacaoNumerica(acum, atual) {
+return atual + acum
+}
+
+const soma = numeros.reduce(operacaoNumerica, 0);
+
+console.log(soma)
